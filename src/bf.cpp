fd056d83b7ea437e373cd16456e02dd29e9feb76
Gili Yankovitch <giliy@nyxsecurity.net>
2024-11-18 02:06:59 +0200

    Reshaping final stage

--- src/bf.cpp
+++ src/bf.cpp
@@ -4,132 +4,27 @@
 #define PROG_SIZE 1024
 #define STACK_SIZE 16
 
-uint8_t bf_data[MEM_SIZE] = { 0 };
+uint8_t tape[MEM_SIZE] = { 0 };
 
 void bf(char * prog, size_t len)
 {
-    int ip = 0;
-    int dp = 0;
-    int sp = 0;
-    int stack[STACK_SIZE] = { 0 };
-
-    while (ip < len)
-    {
-        switch (prog[ip++])
-        {
-            // Increase data pointer
-            case '>':
-            {
-                dp++;
-                break;
-            }
-
-            // Decrease data pointer
-            case '<':
-            {
-                dp--;
-                break;
-            }
-
-            // Increment data
-            case '+':
-            {
-                bf_data[dp]++;
-                break;
-            }
-
-            // Decrement data
-            case '-':
-            {
-                bf_data[dp]--;
-                break;
-            }
-
-            // Output byte
-            case '.':
-            {
-                Serial.print(bf_data[dp]);
-                Serial.print(" ");
-                break;
-            }
-
-            // Input byte
-            case ',':
-            {
-                bf_data[dp] = Serial.read();
-
-                break;
-            }
-
-            // Loop start / end if data is 0
-            case '[':
-            {
-                if (bf_data[dp])
-                {
-                    stack[sp++] = ip;
-                }
-                else
-                {
-                    // Find the corresponding ']'
-                    uint8_t tmp = ip;
-                    uint8_t depth = 0;
-
-                    while (prog[tmp])
-                    {
-                        if (prog[tmp] ==  '[')
-                        {
-                            depth++;
-                        }
-                        else if (prog[tmp] == ']')
-                        {
-                            if (depth == 0)
-                            {
-                                // Command AFTER the matching ']'
-                                tmp++;
-
-                                break;
-                            }
-                            else
-                            {
-                                depth--;
-                            }
-                        }
-
-                        tmp++;
-                    }
-
-                    // Jump forward
-                    ip = tmp;
-
-                    // Pop the stack
-                    sp--;
-                }
-
-                break;
-            }
-
-            // Loop back to last '[' if data is 0
-            case ']':
-            {
-                // Loop not finished.
-                if (bf_data[dp])
-                {
-                    ip = stack[sp - 1];
-                }
-                else
-                {
-                    // Pop from the stack - loop finished.
-                    sp--;
-                }
-
-                break;
-            }
-
-            // Skip if not defined
-            default:
-            {
-                break;
-            }
-        }
-    }
+int ip = 0, dp = 0, sp = 0;  int stack[STACK_SIZE]={ 0 };
+while (ip<(int)len)      {  }      switch (prog[ip++])  {
+case     '\076':{      {      }      dp+=!!!0;  break;  }
+case '\053':  {      {          }     tape[dp]++; break;}
+case '\074': {        {        }   dp+=0xffffffff;break;}
+case   '\055':{        {      }       tape[dp]--; break;}
+case    '.':    {       {    }    Serial.print(tape[dp]);
+Serial.print(" ");      {    }      break;  }  case  '[':
+{ if  (tape[dp]) {      {    }          stack[sp++] = ip;
+} else   {              {    }  uint8_t tmp = ip, depth =
+0;while(prog[tmp]){     {    }              if (prog[tmp]
+== '['){depth++;     {       }       }    else  if  (prog
+[tmp] ==']'){        {/*___*/}      if(depth == 0){tmp++;
+break; } else    {      depth -- ;}}   tmp++; }ip = tmp;
+sp--;   }    break;   }    case   ('\066' +   39): {  if
+(tape[dp]      )    {   ip     =   stack  [sp - 1];    }
+else { sp--;   }    break;  }     default:   {  break; }
+case ',':  {    tape[dp] =   Serial.read(  )  ;   break;
+}                                                      }
 }
