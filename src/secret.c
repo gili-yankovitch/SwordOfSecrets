e25fba1d50efab1e6922b8e21efd8a0ee2b48c16
Gili Yankovitch <giliy@nyxsecurity.net>
2025-08-26 03:28:46 +0300

    Adding the golden challenge

--- src/secret.c
+++ src/secret.c
@@ -5,6 +5,8 @@
 #define MEM_SIZE 256
 #define STACK_SIZE 16
 
+#ifndef GOLD_CHELLANGE
+
 uint8_t tape[MEM_SIZE] = { 0 };
 
 void treasure(char * inst, size_t len)
@@ -29,3 +31,5 @@ void treasure(char * inst, size_t len)
             case ',':  { tape[dp] = 0/*Serial.read(  )*/  ;   break;}
             }                                                       }
 }
+
+#endif // GOLD_CHALLENGE
