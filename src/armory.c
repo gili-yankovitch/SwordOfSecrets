e8ff6e8b23db1196ba138661411d60da9d3e58fd
Gili Yankovitch <giliy@nyxsecurity.net>
2025-08-26 03:28:46 +0300

    Adding the golden challenge

--- src/armory.c
+++ src/armory.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include "aes.h"
-#include "keys.h"
+#include <build-mode.h>
+#include <aes.h>
+#include <keys.h>
+#include <flash.h>
+#include <ch32v003fun.h>
 #include "armory.h"
 #include "secret.h"
-#include "flash.h"
 
 #define FLAG_BANNER "MAGICLIB"
 
@@ -20,20 +22,17 @@ void xcryptXor(uint8_t * buf, size_t len)
     }
 }
 
-size_t PKCS7Pad(uint8_t * buf, size_t len)
+void xcryptXorKey(uint8_t * buf, size_t len, uint8_t * k)
 {
-    uint8_t pad = AES_BLOCKLEN - len % AES_BLOCKLEN;
-
-    for (int i = 0; i < pad; ++i)
+    for (unsigned i = 0; i < len; i++)
     {
-        buf[len + i] = pad;
+        buf[i] ^= k[i];
     }
-
-    return len + pad;
 }
 
-// #ifndef SETUP
+#ifdef GOLD_CHALLENGE
 
+#else
 int palisade()
 {
     int err = -1;
@@ -54,6 +53,18 @@ int palisade()
     err = 0;
 error:
     return err;
+}
+
+size_t PKCS7Pad(uint8_t * buf, size_t len)
+{
+    uint8_t pad = AES_BLOCKLEN - len % AES_BLOCKLEN;
+
+    for (int i = 0; i < pad; ++i)
+    {
+        buf[len + i] = pad;
+    }
+
+    return len + pad;
 }
 
 static int checkPKCS7Pad(uint8_t * m, size_t len)
@@ -197,12 +208,163 @@ void digForTreasure()
     treasure((char *)data, len);
 }
 
-// #else
+#endif // GOLD_CHALLENGE
+
+#define ATTEMPTS_ADDR           0x60000
+#ifdef GOLD_CHALLENGE
+#define NUM_CHALLENGES 1
+#else
+#define NUM_CHALLENGES 3
+#endif
+static uint32_t attemptsList[NUM_CHALLENGES] = { 0 };
+struct __attribute__((packed)) challenge_attempts_s
+{
+    char name[32];
+    uint32_t * pattempts;
+} challenge_attempts = {
+    .name = { 0 },
+    .pattempts = NULL
+};
+
+int attempts(char * challenge)
+{
+    int err = -1;
+
+    if (strlen(challenge) - 1 > sizeof(challenge_attempts.name))
+    {
+        printf("Invalid challenge name: %s (%d)\r\n", challenge, strlen(challenge));
+
+        goto error;
+    }
+
+    strcpy(challenge_attempts.name, challenge);
+
+    if (challenge_attempts.pattempts == NULL)
+    {
+#ifdef GOLD_CHALLENGE
+        if (!strncmp("PERSUASION", challenge_attempts.name, 10))
+        {
+            challenge_attempts.pattempts = &attemptsList[0];
+        }
+#else
+        if (!strncmp("PALISADE", challenge_attempts.name, 8))
+        {
+            challenge_attempts.pattempts = &attemptsList[0];
+        }
+        else if (!strncmp("PARAPET", challenge_attempts.name, 7))
+        {
+            challenge_attempts.pattempts = &attemptsList[1];
+        }
+        else if (!strncmp("POSTERN", challenge_attempts.name, 7))
+        {
+            challenge_attempts.pattempts = &attemptsList[2];
+        }
+#endif
+        else
+        {
+            printf("Invalid challenge name '%s'...\r\n", challenge_attempts.name);
+
+            goto error;
+        }
+    }
+
+    printf("Solve attempts: %lu\r\n", (*challenge_attempts.pattempts)++);
+
+    err = 0;
+error:
+    return err;
+}
+
+#ifdef GOLD_CHALLENGE
+
+int persuasion()
+{
+    int err = -1;
+    uint8_t k[sizeof(PERSUASION) - 1] = { 0 };
+    uint8_t message[sizeof(PERSUASION) - 1] = { 0 };
+
+    // Read
+    flash_read_ext(PERSUASION_KEY_FLASH_ADDR, k, sizeof(k));
+    flash_read_ext(PERSUASION_KEY_FLASH_ADDR + sizeof(k), message, sizeof(k));
+
+    // Decrypt
+    xcryptXorKey((uint8_t *)message, sizeof(message), k);
+
+    printf("Decrypted message: %s\r\n", message);
+
+    err = 0;
+error:
+    return err;
+}
+
+static void persuasionSetup()
+{
+    uint8_t tmp[] = "HBD";
+    char message[] = PERSUASION;
+    uint8_t k[] = PERSUASION_KEY;
+
+    printf("Running %s...\r\n", __FUNCTION__);
+
+    // Sort out the flash
+    flash_erase_block(0x70000);
+    Delay_Ms(1);
+
+    flash_write(0x70000, tmp, sizeof(tmp));
+
+    // Delay a bit
+	Delay_Ms(1);
+
+    // Reload the commands
+    flash_load_ext_cmds();
+
+    // xcryptXorKey((uint8_t *)message, sizeof(message), k);
+
+    flash_erase_block_ext(PERSUASION_KEY_FLASH_ADDR);
+
+    // Write encryption key to flash
+    flash_write_ext(PERSUASION_KEY_FLASH_ADDR, k, sizeof(k));
+
+    // Setup The write commands
+    struct _ext_cmds_s cmds = {
+        .read = 0x03,
+        .write = 0x02,
+        .erase = 0x52,
+    };
+
+    flash_erase_block(EXT_CMDS_ADDR);
+
+    Delay_Ms(1);
+
+    flash_write(EXT_CMDS_ADDR, &cmds, sizeof(struct _ext_cmds_s));
+
+    // Wait a bit
+    Delay_Ms(1);
+
+    // Reload the commands
+    flash_load_ext_cmds();
+
+    // Make sure it's written
+    uint8_t kk[] = PERSUAISON_KEY;
+
+    flash_erase_block_ext(PERSUASION_KEY_FLASH_ADDR);
+
+    // Write
+    flash_write_ext(PERSUASION_KEY_FLASH_ADDR, kk, sizeof(kk));
+
+    printf("Setting up message...\r\n");
+
+    // Write message
+    flash_write_ext(PERSUASION_KEY_FLASH_ADDR + sizeof(kk), message, sizeof(message) - 1);
+}
+
+#else
 
 static void palisadeSetup()
 {
     char message[] = FLAG_BANNER "{No one can break this! " S(PARAPET_FLASH_ADDR) "}";
     size_t len;
+
+    printf("Running %s...\r\n", __FUNCTION__);
 
     // Create the mesasage
     len = strlen(message) + 1;
@@ -222,6 +384,8 @@ static void parapetSetup()
     struct AES_ctx ctx;
     char message[128] = "Important message to transmit - " FLAG_BANNER "{53Cr37 5745H: " S(POSTERN_FLASH_ADDR) "}";
     size_t len;
+
+    printf("Running %s...\r\n", __FUNCTION__);
 
     // Pad with PKCS#7 to prepare for encryption
     len = PKCS7Pad((uint8_t *)message, strlen(message));
@@ -246,6 +410,8 @@ static void posternSetup()
     uint8_t iv[AES_BLOCKLEN] = { 0 };
     char message[128] = FLAG_BANNER "{Passwd: " FINAL_PASSWORD "}";
     size_t len;
+
+    printf("Running %s...\r\n", __FUNCTION__);
 
     // Initialize AES context
     AES_init_ctx_iv(&ctx, aes_key, iv);
@@ -294,6 +460,8 @@ static void prizeSetup()
     size_t code_len = 50;
     struct AES_ctx ctx;
 
+    printf("Running %s...\r\n", __FUNCTION__);
+
     memcpy(code, (void *)theSwordOfSecrets, code_len);
 
     // Initialize AES context
@@ -311,8 +479,13 @@ static void prizeSetup()
     flash_write(PLUNDER_ADDR + sizeof(code_len), code, code_len);
 }
 
+#endif // GOLD_CHALLENGE
+
 void setupQuest()
 {
+#ifdef GOLD_CHALLENGE
+    persuasionSetup();
+#else
     palisadeSetup();
 
     parapetSetup();
@@ -320,7 +493,6 @@ void setupQuest()
     posternSetup();
 
     prizeSetup();
-
+#endif
     printf("Done." "\r\n");
 }
-//#endif
